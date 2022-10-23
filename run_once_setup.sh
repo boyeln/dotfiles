5c182d3cb63b4597b6c710f04b6e6254057e62c1
Boye Borg Nygård <6630430+boyeborg@users.noreply.github.com>
2022-10-23 17:49:09 +0200

    make script idempotent

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -2,19 +2,41 @@
 
 # Install homebrew
 echo "=== Installing Homebrew ==="
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ $(command -v brew) == "" ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew already installed"
+fi
 
 # Install Kitty terminal
-echo "=== Installing Kitty Terminal ==="
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+echo "=== Installing Kitty terminal ==="
+if [[ $(command -v brew) == "" ]]; then
+    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+else
+    echo "Kitty terminal already installed"
+fi
 
 # Install Oh My Zsh
 echo "=== Installing Oh My Zsh ==="
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "Oh My Zsh already installed"
+fi
 
 # Install pure terminal prompt
-mkdir -p "$HOME/.zsh"
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+echo "=== Installing Pure terminal prompt ==="
+if [ ! -d ~/.zsh/pure ]; then
+    mkdir -p "~/.zsh"
+    git clone https://github.com/sindresorhus/pure.git "~/.zsh/pure"
+else
+    echo "Pure terminal prompt already installed"
+fi
 
 # Install Oh My Zsh highlight plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "=== Installing Oh My Zsh highlight plugin ==="
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+    echo "Oh My Zsh highlight plugin already installed"
+fi
